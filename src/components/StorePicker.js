fb33ab110a89b0b2945fa004bec19a811fc7c8fa
unknown <you@example.com>
2017-10-03 19:09:23 -0400

    created first component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+//components can be different in files to be orginzed
+//if you need in a js Modules, must import to every file you need it in
+import React from 'react';
+
+// each Component need a method (Render)
+class StorePicker extends React.Component {
+    render() {
+        return <p>Fuck You Bitch</p>
+    }
+}
+
+export default StorePicker;
