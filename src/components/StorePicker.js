6ba9a4aa2513839fcb6fcc77869ab041754fd7f7
unknown <you@example.com>
2017-10-03 23:18:34 -0400

    worked with routing

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,14 +1,31 @@
 //components can be different in files to be orginzed
 //if you need in a js Modules, must import to every file you need it in
 import React from 'react';
+import { getFunName } from '../helpers'
 
 // each Component need a method (Render)
 class StorePicker extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+    // SEE onSubmit goToStore below are simular constructor
+
+    goToStore(event) {
+        event.preventDefault();
+        console.log("You change the URL");
+        //1st grab text from box
+        console.log(this.storeInput.value);
+        //2nd going to transition from to store/:storeId
+    }
+
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
             <h2>Please Enter A Store</h2> {}
-            <input type='text' required placeholder='Store Name'/>
+            <input type='text' required placeholder='Store Name' defaultValue={getFunName()} 
+                ref={(input) => {this.storeInput = input}}/>
             <button type='submit'>Visit Store</button>
             </form> //JSX only return ONE parent elements 
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom'; // import ReactDOM from 'react-dom'; -- entire package
+import { BrowserRouter, Match, Miss} from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
 import Header from './components/Header';
@@ -7,6 +9,20 @@ import Fish from './components/Fish';
 import Order from './components/Order';
 import Inventory from './components/Inventory';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+
+const Root = () => {
+    return (
+        <BrowserRouter>
+        <div>
+          <Match exactly pattern='/' component={StorePicker}/>
+          <Match pattern='/store/:storeId' component={App}/>
+          <Miss component={NotFound}/>
+        </div>
+        </BrowserRouter>
+    )
+}
 
 // ES6 pretty much like function render() {} JS
-render(<App/>, document.querySelector('#main'));
+render(<Root/>, document.querySelector('#main'));
